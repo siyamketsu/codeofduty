e84d8f8e0f48246c6283675826b2c96d0ab5c3f8
siyamketsu <93192591+siyamketsu@users.noreply.github.com>
2022-08-19 09:48:40 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,120 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+
+let navScroll = document.querySelector('.header');
+
+window.addEventListener('scroll', function () {
+    let navOnScroll = window.scrollY > 0;
+    navScroll.classList.toggle('scroll-active', navOnScroll);
+  })
+
+// var swiper = new Swiper(".product-slider", {
+//     loop:true,
+//     spaceBetween: 20,
+//     autoplay: {
+//         delay: 7500,
+//         disableOnInteraction: false,
+//     },
+//     centeredSlides: true,
+//     breakpoints: {
+//       0: {
+//         slidesPerView: 1,
+//       },
+//       768: {
+//         slidesPerView: 2,
+//       },
+//       1020: {
+//         slidesPerView: 3,
+//       },
+//     },
+// });
+
+// var swiper = new Swiper(".review-slider", {
+//     loop:true,
+//     spaceBetween: 20,
+//     autoplay: {
+//         delay: 7500,
+//         disableOnInteraction: false,
+//     },
+//     centeredSlides: true,
+//     breakpoints: {
+//       0: {
+//         slidesPerView: 1,
+//       },
+//       768: {
+//         slidesPerView: 2,
+//       },
+//       1020: {
+//         slidesPerView: 3,
+//       },
+//     },
+// });
+
+
+
+
+// SWITCH LOGIN/SIGN UP
+let signUpBtn = document.querySelector(".signup-btn");
+let signInBtn = document.querySelector(".signin-btn");
+
+signUpBtn.onclick = function (){
+  loginForm.classList.add("changes");
+}
+signInBtn.onclick = function (){
+  loginForm.classList.remove("changes");
+}
+
+// SELL FORM 
+let showForm = document.querySelector("#sell");
+let hideForm = document.querySelector(".log-close");
+
+
+let sellForm = document.querySelector(".sell-buy-form");
+
+showForm.onclick = function (){
+    sellForm.style.display = "block";
+}
+hideForm.onclick = function (){
+    sellForm.style.display = "none";
+}
